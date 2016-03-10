7863f00c6c27cd6d02abf31a42fb7937817c4656
Rob Chiarelli <rob.chiarelli@nyu.edu>
2016-03-10 16:00:30 -0500

    changed unordered_map to map. compilation works now

--- encrypt.cpp
+++ encrypt.cpp
@@ -23,39 +23,39 @@ void print_freqs(const vector<int> &cipher_text);
 vector<int> encode(string s);
 
 const vector<string> plaintexts = {
-  "sconced pouch bogart lights coastal philip nonexplosive shriller outstripping "
-  "underbidding nightshirts colly editorializer trembler unresistant resins anthrax "
-  "polypus research parapets gratuitous corespondent pyrometer breveted psychoneurosis "
-  "scoutings almightily endoscopes cyanosis kayaker hake william blunted incompressibility "
-  "lacer cumquat aniline agileness academe obstacle toothpick nondistribution rebukes concertizes "
-  "industrialist plenipotentiary swagmen kevils dredge ostensible atavistic p",
+	"sconced pouch bogart lights coastal philip nonexplosive shriller outstripping "
+	"underbidding nightshirts colly editorializer trembler unresistant resins anthrax "
+	"polypus research parapets gratuitous corespondent pyrometer breveted psychoneurosis "
+	"scoutings almightily endoscopes cyanosis kayaker hake william blunted incompressibility "
+	"lacer cumquat aniline agileness academe obstacle toothpick nondistribution rebukes concertizes "
+	"industrialist plenipotentiary swagmen kevils dredge ostensible atavistic p",
 
-  "revelation revering rightest impersonalize juliennes scientists reemphasizing "
-  "propose crony bald pampering discharged lincoln authoresses interacted laked "
-  "bedmaker intolerably beltlines warningly worldliness serologic bottom guessed "
-  "hangup vitiates snaky polypous manifolding sweatshirt divisiveness decapitation "
-  "musketry versers pizzas aperies reorganizes fender presentations thereuntil fly "
-  "entrapped causewayed shaped freemasonry nudging efflorescence hydrated zazen exegeses fracas unprogressivel",
+	"revelation revering rightest impersonalize juliennes scientists reemphasizing "
+	"propose crony bald pampering discharged lincoln authoresses interacted laked "
+	"bedmaker intolerably beltlines warningly worldliness serologic bottom guessed "
+	"hangup vitiates snaky polypous manifolding sweatshirt divisiveness decapitation "
+	"musketry versers pizzas aperies reorganizes fender presentations thereuntil fly "
+	"entrapped causewayed shaped freemasonry nudging efflorescence hydrated zazen exegeses fracas unprogressivel",
 
-  "boca ingestion financed indexer generalships boldfaces boughed tesla videotext "
-  "expiation brasil kinglets duality rattlesnakes mailability valvelet whimperingly "
-  "corralled stench fatal inapplicably uncourageous bubblers req jesse foetor bulgaria "
-  "hueless pickwicks intrans gargles purgations subvarieties pettier caste decongestive "
-  "replanned continual bribed pirog learning currier careers rustling swankily onetime "
-  "prearranges stowage responder inwrapped coign concubines gyrus delta tripled sleetier m",
+	"boca ingestion financed indexer generalships boldfaces boughed tesla videotext "
+	"expiation brasil kinglets duality rattlesnakes mailability valvelet whimperingly "
+	"corralled stench fatal inapplicably uncourageous bubblers req jesse foetor bulgaria "
+	"hueless pickwicks intrans gargles purgations subvarieties pettier caste decongestive "
+	"replanned continual bribed pirog learning currier careers rustling swankily onetime "
+	"prearranges stowage responder inwrapped coign concubines gyrus delta tripled sleetier m",
 
-  "allocated demonstration cocoanuts imprecisions mikado skewer ennobled cathect universalizes "
-  "lucidity soldierly calor narthexes jiggling mutinousness relight mistook electra ogles chirk "
-  "unsympathetic indorsed theomania gaper moths aerospace riboflavin sensorium teariest luckiest "
-  "dither subparts purslane gloam dictatory conversed confides medullary fatsos barked yank chained "
-  "changes magicians movables ravenousness dipsomaniac budded windjammers stayers dixie tepidities desexualization boodled dile",
+	"allocated demonstration cocoanuts imprecisions mikado skewer ennobled cathect universalizes "
+	"lucidity soldierly calor narthexes jiggling mutinousness relight mistook electra ogles chirk "
+	"unsympathetic indorsed theomania gaper moths aerospace riboflavin sensorium teariest luckiest "
+	"dither subparts purslane gloam dictatory conversed confides medullary fatsos barked yank chained "
+	"changes magicians movables ravenousness dipsomaniac budded windjammers stayers dixie tepidities desexualization boodled dile",
 
-  "shellackers ballets unselfishly meditatively titaness highballed serenaders ramshorns "
-  "bottlenecks clipsheet unscriptural empoisoned flocking kantians ostensibilities heigh "
-  "hydrodynamics qualifier million unlading distributed crinkliest conte germ certifier weaklings "
-  "nickeled watson cutis prenticed debauchery variously puccini burgess landfalls nonsecular "
-  "manipulability easterlies encirclements nescient imperceptive dentally sudsers reediness "
-  "polemical honeybun bedrock anklebones brothering narks"
+	"shellackers ballets unselfishly meditatively titaness highballed serenaders ramshorns "
+	"bottlenecks clipsheet unscriptural empoisoned flocking kantians ostensibilities heigh "
+	"hydrodynamics qualifier million unlading distributed crinkliest conte germ certifier weaklings "
+	"nickeled watson cutis prenticed debauchery variously puccini burgess landfalls nonsecular "
+	"manipulability easterlies encirclements nescient imperceptive dentally sudsers reediness "
+	"polemical honeybun bedrock anklebones brothering narks"
 };
 
 int main()
@@ -63,38 +63,41 @@ int main()
 	string message;
 	vector<int> cipher_text;
 	map<char, vector<int>> key;
-  set<string> dict;
+	set<string> dict;
 
 	srand(time(NULL));
 	create_key(key);
-  index_dict("english_words.txt", dict);
-  cout << "There are " << dict.size() << " words in the dictionary" << endl;
+	
+	index_dict("english_words.txt", dict);
+	cout << "There are " << dict.size() << " words in the dictionary" << endl;
 
-  // encrypt(message, cipher_text, key);
-  // cout << decrypt(cipher_text, key) << endl;
-  unordered_map<vector<int>, vector<string> > encodings;
-  for(auto i = dict.begin(); i != dict.end(); i++) {
-    vector<int> e = encode(*i);
-    encodings[e].push_back(*i);
-  }
-  for(auto i = encodings.begin(), j = advance(encodings.begin() , 10); i != j; i++) {
-    for(int k : i->first) cout << k << " ";
-    cout << ": ";
-    for(string s : i->second) cout << s << ", ";
-    cout << endl;
-  }
+	// encrypt(message, cipher_text, key);
+	// cout << decrypt(cipher_text, key) << endl;
+	//unordered_map<vector<int>, vector<string> > encodings;
+	map<vector<int>, vector<string> > encodings;
+	for(auto i = dict.begin(); i != dict.end(); i++) {
+		vector<int> e = encode(*i);
+		encodings[e].push_back(*i);
+	}
+	//for(auto i = encodings.begin(), j = advance(encodings.begin(), 10); i != j; i++) {
+	for(auto i = encodings.begin(), j = next(i, 10); i != j; i++) {
+		for(int k : i->first) cout << k << " ";
+		cout << ": ";
+		for(string s : i->second) cout << s << ", ";
+		cout << endl;
+	}
 }
 
 void create_key(map<char, vector<int>> &key)
 {
-  static string letters = "abcdefghijklmnopqrstuvwxyz";
-  static int freqs[] = {8, 1, 3, 4, 13, 2, 2, 6, 7, 1, 1, 4, 2, 7, 8, 2, 1, 6, 6, 9, 3, 1, 2, 1, 2, 1};
-  vector<int> rand_nums;
-  for (int i = 0; i <= 102; i++) rand_nums.push_back(i);
-  random_shuffle(rand_nums.begin(), rand_nums.end());
-  int rand_index = 0;
+	static string letters = "abcdefghijklmnopqrstuvwxyz";
+	static int freqs[] = {8, 1, 3, 4, 13, 2, 2, 6, 7, 1, 1, 4, 2, 7, 8, 2, 1, 6, 6, 9, 3, 1, 2, 1, 2, 1};
+	vector<int> rand_nums;
+	for (int i = 0; i <= 102; i++) rand_nums.push_back(i);
+	random_shuffle(rand_nums.begin(), rand_nums.end());
+	int rand_index = 0;
 
-  for (int i = 0; i < letters.size(); i++)
+	for (int i = 0; i < letters.size(); i++)
 	{
 		vector<int> key_list;
 		for (int j = 0; j < freqs[i]; j++)
@@ -103,7 +106,7 @@ void create_key(map<char, vector<int>> &key)
 		}
 
     key[letters[i]] = key_list;
-  }
+	}
 }
 
 // Print out key
@@ -118,48 +121,48 @@ void print_key(map<char, vector<int>> &key)
 
 void print_ciphertext(vector<int> &cipher_text)
 {
-  for (int i = 0; i < cipher_text.size(); i++) {
-    if (cipher_text[i] != -1) {
+	for (int i = 0; i < cipher_text.size(); i++) {
+		if (cipher_text[i] != -1) {
 			cout << cipher_text[i];
-      if (i != cipher_text.size() - 1 && cipher_text[i+1] != -1) cout << ",";
-    }
+			if (i != cipher_text.size() - 1 && cipher_text[i+1] != -1) cout << ",";
+		}
 		else cout << " ";
-  }
+	}
 	cout << endl;
 }
 
 void encrypt(string &pt, vector<int> &ct, map<char, vector<int>> &key)
 {
-  int string_counter = 0;
-  for (char c : pt) {
-      if (isspace(c)) ct.push_back(-1);
-      else {
-        vector<int> temp = key.find(c)->second;
-        // This is the scheduling algo
-        ct.push_back(temp[string_counter % temp.size()]);
-      }
-      string_counter++;
+	int string_counter = 0;
+	for (char c : pt) {
+		if (isspace(c)) ct.push_back(-1);
+		else {
+			vector<int> temp = key.find(c)->second;
+			// This is the scheduling algo
+			ct.push_back(temp[string_counter % temp.size()]);
+		}
+		string_counter++;
     }
 }
 
 string decrypt(vector<int> &ct, map<char, vector<int>> &key)
 {
-  vector<char> rkey(102);
-  for(char i = 'a'; i <= 'z'; i++) {
-    vector<int> curr = key.find(i)->second;
-    for(int j = 0; j < curr.size(); j++) {
-      rkey[curr[j]] = i;
-    }
-  }
-  string pt = "";
-  for(int i = 0; i < ct.size(); i++) {
-    if(ct[i] < 0) {
-      pt += ' ';
-      continue;
-    }
-    pt += rkey[ct[i]];
-  }
-  return pt;
+	vector<char> rkey(102);
+	for(char i = 'a'; i <= 'z'; i++) {
+		vector<int> curr = key.find(i)->second;
+		for(int j = 0; j < curr.size(); j++) {
+			rkey[curr[j]] = i;
+		}
+	}
+	string pt = "";
+	for(int i = 0; i < ct.size(); i++) {
+		if(ct[i] < 0) {
+			pt += ' ';
+			continue;
+		}
+		pt += rkey[ct[i]];
+	}
+	return pt;
 }
 
 void delimit_ciphertext(string ct, vector<int> &ct_nums)
@@ -167,54 +170,54 @@ void delimit_ciphertext(string ct, vector<int> &ct_nums)
 	istringstream ss(ct);
 	string word;
 	while(getline(ss, word, ' ')) {
-    istringstream ss_token(word);
-    string character;
-    while (getline(ss_token, character, ',')) {
-      ct_nums.push_back(stoi(character));
-    }
-    if (!ss.eof()) ct_nums.push_back(-1);
-  }
+		istringstream ss_token(word);
+		string character;
+		while (getline(ss_token, character, ',')) {
+			ct_nums.push_back(stoi(character));
+		}
+		if (!ss.eof()) ct_nums.push_back(-1);
+	}
 }
 
 void index_dict(const string &path, set<string> &dict)
 {
-  ifstream ifs(path);
-  if(!ifs) {
-    cerr << "Error opening " << path << endl;
-    exit(1);
-  }
-  string line;
-  while(getline(ifs, line)) dict.insert(line);
+	ifstream ifs(path);
+	if(!ifs) {
+		cerr << "Error opening " << path << endl;
+		exit(1);
+	}
+	string line;
+	while(getline(ifs, line)) dict.insert(line);
 }
 
 void print_freqs(const vector<int> &cipher_text)
 {
-  vector<pair<int, int> > freqs(103);
-  for(int i = 0; i < freqs.size(); i++) {
-    freqs[i].first = i;
-    freqs[i].second = 0;
-  }
-  for(int i = 0; i < cipher_text.size(); i++) {
-    if(cipher_text[i] > 0) freqs[cipher_text[i]].second++;
-  }
-  sort(freqs.begin(), freqs.end(), [](pair<int,int> x, pair<int, int> y){return x.second > y.second;});
-  for(int i = 0; i < freqs.size(); i++) cout << freqs[i].first << ": " << freqs[i].second << endl;
+	vector<pair<int, int> > freqs(103);
+	for(int i = 0; i < freqs.size(); i++) {
+		freqs[i].first = i;
+		freqs[i].second = 0;
+	}
+	for(int i = 0; i < cipher_text.size(); i++) {
+		if(cipher_text[i] > 0) freqs[cipher_text[i]].second++;
+	}
+	sort(freqs.begin(), freqs.end(), [](pair<int,int> x, pair<int, int> y){return x.second > y.second;});
+	for(int i = 0; i < freqs.size(); i++) cout << freqs[i].first << ": " << freqs[i].second << endl;
 }
 
 vector<int> encode(string s)
 {
-  // seen keeps track of letters we saw already and what we mapped them to
-  // 0..25 maps to a..z
-  vector<int> encoding;
-  vector<int> seen(26, -1);
-  int mapping = 0;
-  for(int i = 0; i < s.size(); i++) {
-    if(seen[s[i]-'a'] < 0) {
-      seen[s[i]-'a'] = mapping;
-      encoding.push_back(mapping);
-      mapping++;
-    }
-    else encoding.push_back(seen[s[i]-'a']);
-  }
-  return encoding;
+	// seen keeps track of letters we saw already and what we mapped them to
+	// 0..25 maps to a..z
+	vector<int> encoding;
+	vector<int> seen(26, -1);
+	int mapping = 0;
+	for(int i = 0; i < s.size(); i++) {
+		if(seen[s[i]-'a'] < 0) {
+			seen[s[i]-'a'] = mapping;
+			encoding.push_back(mapping);
+			mapping++;
+		}
+		else encoding.push_back(seen[s[i]-'a']);
+	}
+	return encoding;
 }
