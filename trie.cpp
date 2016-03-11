4b5b211953ceb03dec61249c69ae9e108ef9f6c6
Salomon <si612@nyu.edu>
2016-03-10 23:53:32 -0500

    cleaned up trie; added destructor

--- trie.cpp
+++ trie.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Node {
 public:
     Node() { mContent = ' '; mMarker = false; }
-    ~Node() {}
+    ~Node() { for(Node* n : children) delete n; }
     char content() { return mContent; }
     void setContent(char c) { mContent = c; }
     bool wordMarker() { return mMarker; }
@@ -34,15 +34,12 @@ private:
 
 Node* Node::findChild(char c)
 {
-    for ( int i = 0; i < mChildren.size(); i++ )
-    {
+    for (int i = 0; i < mChildren.size(); i++) {
         Node* tmp = mChildren.at(i);
-        if ( tmp->content() == c )
-        {
+        if (tmp->content() == c) {
             return tmp;
         }
     }
-
     return NULL;
 }
 
@@ -54,95 +51,48 @@ Trie::Trie()
 Trie::~Trie()
 {
     // Free memory
+    delete root;
 }
 
 void Trie::addWord(string s)
 {
     Node* current = root;
 
-    if ( s.length() == 0 )
-    {
+    if (s.length() == 0) {
         current->setWordMarker(); // an empty word
         return;
     }
 
-    for ( int i = 0; i < s.length(); i++ )
-    {        
+    for (int i = 0; i < s.length(); i++) {
         Node* child = current->findChild(s[i]);
-        if ( child != NULL )
-        {
+        if (child != NULL) {
             current = child;
         }
-        else
-        {
+        else {
             Node* tmp = new Node();
             tmp->setContent(s[i]);
             current->appendChild(tmp);
             current = tmp;
         }
-        if ( i == s.length() - 1 )
+        if (i == s.length() - 1)
             current->setWordMarker();
     }
 }
 
-
 bool Trie::searchWord(string s)
 {
     Node* current = root;
-
-    while ( current != NULL )
-    {
-        for ( int i = 0; i < s.length(); i++ )
-        {
+    while (current != NULL) {
+        for (int i = 0; i < s.length(); i++) {
             Node* tmp = current->findChild(s[i]);
-            if ( tmp == NULL )
+            if (tmp == NULL)
                 return false;
             current = tmp;
         }
-
-        if ( current->wordMarker() )
+        if (current->wordMarker())
             return true;
         else
             return false;
     }
-
     return false;
-}
-
-
-// Test program
-int main()
-{
-    Trie* trie = new Trie();
-	ifstream ifs("english_words.txt");
-	string line;
-	
-	while (getline(ifs, line)) trie->addWord(line);
-	
-	if ( trie->searchWord("reports") )
-        cout << "Found it" << endl;
-    else
-		cout << "Failure" << endl;
-    /*
-    trie->addWord("Hello");
-    trie->addWord("Balloon");
-    trie->addWord("Ball");
-
-    if ( trie->searchWord("Hell") )
-        cout << "Found Hell" << endl;
-
-    if ( trie->searchWord("Hello") )
-        cout << "Found Hello" << endl;
-
-    if ( trie->searchWord("Helloo") )
-        cout << "Found Helloo" << endl;
-
-    if ( trie->searchWord("Ball") )
-        cout << "Found Ball" << endl;
-
-    if ( trie->searchWord("Balloon") )
-        cout << "Found Balloon" << endl;
-	*/
-
-    delete trie;
 }
